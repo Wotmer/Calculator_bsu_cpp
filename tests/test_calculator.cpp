24bc6654fc82d65131ec19a7fd822afd31365014
Maksim Bantsevich <163719943+Wotmer@users.noreply.github.com>
2024-12-30 23:36:26 +0300

    Update test_calculator.cpp
    добавил тесты для дополнительных функций

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <include/expression_parser.h>
 #include <include/calculator.h>
 
 void testCalculator() {
@@ -14,7 +13,19 @@ void testCalculator() {
         // Унарные операции
         std::cout << "Тест 7 (_16): " << Calculator::calculateUnary(16, '_') << " (Ожидается: 4)" << std::endl;
         std::cout << "Тест 8 (|(-5)): " << Calculator::calculateUnary(-5, '|') << " (Ожидается: 5)" << std::endl;
-
+        std::string a = "1111111111111111111111111111111111";
+        std::cout << "Тест 9 (1111111111111111111111111111111111 + 1111111111111111111111111111111111): "
+        << Calculator::LongSum(a, a) << " (Ожидается: 2222222222222222222222222222222222)" << std::endl;
+        a = "999999999999999999999999999999999999999999999";
+        std::string b = "999999999999999999999999999999999999999999999";
+        std::cout << "Тест 9 (1111111111111111111111111111111111 + 1111111111111111111111111111111111111111111): "
+        << Calculator::LongSum(a, b) << " (Ожидается: 1001111111111111111111111111111111111111111110)" << std::endl;
+        std::cout << "Тест 10 (делители числа 20935093): "; Calculator::Factorize(20935093); std::cout << " (Ожидается: 19 1 151 1 7297 1)" << std::endl;
+        std::cout << "Тест 11 (делители числа 214990848): "; Calculator::Factorize(214990848); std::cout << " (Ожидается: 2 15 3 8)" << std::endl;
+        std::cout << "Тест 12 (корни выражения x^2 + 4x + 4): "; Calculator::SolveQuadratic(1, 4, 4); std::cout << " (Ожидается: Один корень -2)" << std::endl;
+        std::cout << "Тест 13 (корни выражения x^2 + 4x + 5): "; Calculator::SolveQuadratic(1, 4, 5); std::cout << " (Ожидается: Нет корней)" << std::endl;
+        std::cout << "Тест 14 (корни выражения 0): "; Calculator::SolveQuadratic(0, 0, 0); std::cout << " (Ожидается: Бесконечно много корней)" << std::endl;
+        std::cout << "Тест 15 (корни выражения x^2 - 5x + 6): "; Calculator::SolveQuadratic(1, -5, 6); std::cout << " (Ожидается: Два корня 2 3)" << std::endl;
     } catch (const std::invalid_argument& e) {
         std::cerr << "Поймана ошибка в тестах Calculator: " << e.what() << std::endl;
     }
